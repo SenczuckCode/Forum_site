566c69686ec248e7b5c027f920e9cf7b8882e840
SenczuckCode <lucas_senczuck@estudante.sesisenai.org.br>
2026-04-29 09:56:31 -0300

    Up-login/script feito sujeito a mudanças

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-let cadastro = false;
+function validarLogin(event) {
+    event.preventDefault();
+    
+    const usuarioInput = document.getElementById("usuario");
+    const senhaInput = document.getElementById("senha");
+    const mensagemErro = document.getElementById("emailHelp");
 
-let email = document.getElementById("email").value;
-let senha = document.getElementById("senha").value;
+
+    const usuarioFixo = "admin";
+    const senhaFixa = "1234";
+
+    
+    if (usuarioInput.value === "" || senhaInput.value === "") {
+        mensagemErro.innerText = "Preencha todos os campos.";
+        return; 
+    }
+
+    
+    if (usuarioInput.value === usuarioFixo && senhaInput.value === senhaFixa) {
+        mensagemErro.innerText = "Login realizado!";
+        mensagemErro.style.color = "#2ecc71"; 
+
+        setTimeout(() => {
+            window.location.href = "forum.html";
+        }, 1000);
+    } 
+ 
+    else {
+        mensagemErro.innerText = "Usuário ou senha incorretos";
+        mensagemErro.style.color = "red";
+        senhaInput.value = "";
+    }
+}
